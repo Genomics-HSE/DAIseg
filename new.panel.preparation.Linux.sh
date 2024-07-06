0173b909ce7288169d64a2a4d384209b5c828830
ekrez <84871963+ekrez@users.noreply.github.com>
2024-07-06 09:24:48 +0300

    Update new.panel.preparation.Linux.sh

--- new.panel.preparation.Linux.sh
+++ new.panel.preparation.Linux.sh
@@ -3,12 +3,16 @@
 #change the following names and directories
 CHR=$1
 bed=$4
-panel=$5
+NAME1000=$5
+n1=$6
+n2=$7
+n3=$8
+panel=$9
 
-NAME1000=/media/scglab/T7/Work/Data/1000GP/${CHR}/ALL.chr${CHR}.phase3_shapeit2_mvncall_integrated_v5b.20130502.genotypes.vcf.gz #name of the 1000GP vcf files
-n1=/media/scglab/T7/Work/data/neand/33.19/chr${CHR}_mq25_mapab100.vcf.gz
-n2=/media/scglab/T7/Work/data/neand/altai/chr${CHR}_mq25_mapab100.vcf.gz
-n3=/media/scglab/T7/Work/data/neand/ChagyrskayaOkladnikov/split.${CHR}.vcf.gz
+#NAME1000=/media/scglab/T7/Work/Data/1000GP/${CHR}/ALL.chr${CHR}.phase3_shapeit2_mvncall_integrated_v5b.20130502.genotypes.vcf.gz #name of the 1000GP vcf files
+#n1=/media/scglab/T7/Work/data/neand/33.19/chr${CHR}_mq25_mapab100.vcf.gz
+#n2=/media/scglab/T7/Work/data/neand/altai/chr${CHR}_mq25_mapab100.vcf.gz
+#n3=/media/scglab/T7/Work/data/neand/ChagyrskayaOkladnikov/split.${CHR}.vcf.gz
 
 
 cat $3 $2 > samples.for.hmm.txt
@@ -83,8 +87,8 @@ tabix -p vcf 3.vcf.gz
 
 
 echo "DAIseg: Concatenating"
-bcftools concat 3.vcf.gz 1.vcf.gz|bcftools sort |bcftools view -v snps| bcftools annotate -x INFO  -Oz -o ${5}
-tabix -p vcf ${5} 
+bcftools concat 3.vcf.gz 1.vcf.gz|bcftools sort |bcftools view -v snps| bcftools annotate -x INFO  -Oz -o ${9}
+tabix -p vcf ${9} 
 
 rm extra.pos.chr${CHR}.txt
 rm int.txt
