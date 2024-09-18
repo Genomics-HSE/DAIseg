daa6e9bda6091a26fe0ffbbbc1b583bfa892494e
ekrez <84871963+ekrez@users.noreply.github.com>
2024-09-18 14:43:26 +0300

    Update full.preparation.sh

--- full.preparation.sh
+++ full.preparation.sh
@@ -8,28 +8,28 @@ n2=$5
 n3=$6
 GP1000=$7
 
-out=$8
-obs=$9
+obs=$8
+out=$9
 arch=${10}
+
 outfilevcf=${11}
 outtxt=${12}
+aa=${13}
 
 
 
-
-cd $1
+cd preparations
 
 
 ./archaic.covering.sh ${CHR} ${bed} ${n1} ${n2} ${n3}
 
 
-./ancestral.alleles.sh ${CHR} ${GP1000}
 
 ./new.panel.preparation.Linux.sh ${CHR} ${out} ${obs} ${bed} ${GP1000} ${n1} ${n2} ${n3} ${outfilevcf}
 
 
-./new.make.obs.sh ${CHR} ${outfilevcf} ${obs} ${out} ${arch}  ./Ancestral.Alleles/hg19.AA.chr${CHR}.txt ./regions/chr${CHR}.hg19.bed ${outtxt}
+./new.make.obs.sh ${CHR} ${outfilevcf} ${obs} ${out} ${arch}  ${aa} ${bed} ${outtxt}
 
-cd ../
+
 
 
