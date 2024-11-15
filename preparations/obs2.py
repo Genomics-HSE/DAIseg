712ed95a8e97804a44ee4d42dc23c4e396e01f3d
ekrez <84871963+ekrez@users.noreply.github.com>
2024-11-15 12:59:35 +0300

    Add files via upload

--- preparations/obs2.py
+++ preparations/obs2.py
@@ -11,10 +11,10 @@ f_out = sys.argv[5]
 domain = usfl.read_bed(f_bed)
 dct_all = usfl.main_read(f, f_aa)
 
-
+print(len(dct_all.keys()))
 
 L=1000
-
+k=0
 with open(f_out,'w') as f1:
     f1.write('#POSITIONS\t#REF\t#ALT\tANCESTRAL\t#OUTGROUP\t#ARCHAIC\t#OBSERVATIONS\n')
     for i  in dct_all.keys():
@@ -24,7 +24,27 @@ with open(f_out,'w') as f1:
         if s2=='':
             s2+='.'
         s3=str(j['Obs']).replace('[','').replace(']','').replace(',','')
-        f1.write(str(i)+'\t'+str(j['REF'])+'\t'+str(j['ALT'])+'\t'+str(j['AA'])+'\t'+s1+'\t'+s2+'\t'+s3+'\n')
+        
+        flag=[]
+        if j['Archaic']==[]:
+            for o in set(j['Obs']):
+                if o in set(j['Outgroup']):
+                    flag.append(True)
+            if flag==[True for o in set(j['Obs'])]:
+                k+=1
+   
+            else:
+                f1.write(str(i)+'\t'+str(j['REF'])+'\t'+str(j['ALT'])+'\t'+str(j['AA'])+'\t'+s1+'\t'+s2+'\t'+s3+'\n')
+        else:
+            for o in set(j['Obs']):
+                if (o in set(j['Outgroup'])) and (o in set(j['Archaic'])):
+                    flag.append(True)
+            if flag==[True for o in set(j['Obs'])]:
+                k+=1
+   
+            else:
+                f1.write(str(i)+'\t'+str(j['REF'])+'\t'+str(j['ALT'])+'\t'+str(j['AA'])+'\t'+s1+'\t'+s2+'\t'+s3+'\n')        
+                
 
 
 
