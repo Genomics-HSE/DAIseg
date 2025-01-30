5b7259d81a8b6a634d9e417b8720153d5475e2c3
ekrez <84871963+ekrez@users.noreply.github.com>
2025-01-30 10:37:28 +0300

    Add files via upload

--- dai.seg.py
+++ dai.seg.py
@@ -36,7 +36,7 @@ args = parser.parse_args()
 dict_all = usfl.main_read2(args.prepared_file)
 domain = usfl.read_bed(args.bed)
 seq_start, seq_end = domain[0][0], domain[-1][1]
-N = 2 # number of hidden states
+N = 3 # number of hidden states
 GEN_time, MU, RR, L, Lambda_0 = usfl.read_par_HMM(args.HMM_par)
 n_eu = len(dict_all[list(dict_all.keys())[0]]['Obs'])
 
@@ -96,15 +96,13 @@ for i in range(len(len_mas)):
 
 def run_daiseg(lmbd_opt,seq, n_st, idx, start, ar_cover):
     d = MU * L
-    A = HMM.initA(L,RR, lmbd_opt[4]/d, lmbd_opt[3])
     
+    A = HMM.initA( lmbd_opt[4], lmbd_opt[5], p1=0.05, p2=0.05, L=L, r=RR)    
+    B = HMM.initB(MU, L, lmbd_opt[1]/d, lmbd_opt[0]/d, lmbd_opt[2]/d, lmbd_opt[3]/d,n_st)
 
-    
-    B_our_mas = np.array([HMM.initB_arch_cover(MU,L, lmbd_opt, n_st, 0.1+i*0.1) for i in range(10)])
-    B_Skov = HMM.initBwN(L, lmbd_opt[0:3], n_st)
-    P=[0.97, 0.03]
+    P = [0.9, 0.05, 0.05]
 
-    tracts_HMM =  HMM.get_HMM_tracts(HMM.viterbi_modified(seq [idx], P, A, B_our_mas, B_Skov, ar_cover))
+    tracts_HMM =  HMM.get_HMM_tracts(HMM.viterbi_modified(seq [idx], P, A,B))
 
     for k in range(N):
        for j in range(len(tracts_HMM[k])):
@@ -118,7 +116,7 @@ def run_daiseg_all(lmbd_0):
 
     
     for idx in range(0, n_eu):    
-        tracts_HMM=[[],[]]
+        tracts_HMM = [[] for _ in range(N)]
         for i in range(len(SEQ_mas)):
             tr=run_daiseg(lmbd_0, SEQ_mas[i], N_st, idx, seq_start_mas[i], arch_cover[i])
             for j in range(N):   
@@ -131,65 +129,16 @@ def run_daiseg_all(lmbd_0):
 
 
 
-cut_off=args.cut_off
-def run_daiseg_posterior(lmbd_opt,seq, n_st, idx, start, ar_cover, gaps_numbers, cut_off):
-    d = MU * L
-    A = HMM.initA(L,RR, lmbd_opt[4]/d, lmbd_opt[3])
-    
-
-    
-    B_our_mas = np.array([HMM.initB_arch_cover(MU,L, lmbd_opt, n_st, 0.1+i*0.1) for i in range(10)])
-    B_Skov = HMM.initBwN(L, lmbd_opt[0:3], n_st)
-    P=[0.97, 0.03]
-
-    tracts_HMM =  HMM.get_HMM_tracts(HMM.posterior(seq [idx], P, A, B_our_mas, B_Skov, ar_cover, gaps_numbers, cut_off))
-
-    for k in range(N):
-       for j in range(len(tracts_HMM[k])):
-           tracts_HMM[k][j][0]= L * tracts_HMM[k][j][0]+start
-           tracts_HMM[k][j][1]= L * (tracts_HMM[k][j][1]+1)+start-1
-
-    return tracts_HMM
-
-
-def run_daiseg_all_posterior(lmbd_0, cut_off, gaps_numbers):
-    tracts_HMM_mas=[]
-
-    
-    for idx in range(0, n_eu):    
-        tracts_HMM=[[],[]]
-        for i in range(len(SEQ_mas)):
-            tr=run_daiseg_posterior(lmbd_0, SEQ_mas[i], N_st, idx, seq_start_mas[i], arch_cover[i], gaps_numbers, cut_off)
-            for j in range(N):   
-               for k in tr[j]:             
-                   tracts_HMM[j].append( k )
- 
-
-        tracts_HMM_mas.append([tracts_HMM[j] for j in range(N)])
-    return tracts_HMM_mas
-
-def EM_gaps(seq, lambda_0, n_st, cover):
-    return EM.EM_algorithm_gaps(P, seq, n_st, MU, RR, lambda_0, epsilon, L, int(args.EM_steps), gaps_numbers, cover )
-
-P=[0.95, 0.05]
 
 epsilon = 1e-8
 
 
 if args.EM=='no': 
-    if args.decoding=='viterbi':
-        Tracts_HMM_mas = run_daiseg_all(Lambda_0)
-    else:
-        Tracts_HMM_mas=run_daiseg_all_posterior(Lambda_0, cut_off, gaps_numbers)
 
-if args.EM=='yes': 
+    Tracts_HMM_mas = run_daiseg_all(Lambda_0)
 
 
-    Lambda_opt = EM_gaps(np.array(SEQ[0 : (args.EM_samples+1)]), Lambda_0, N_st, cover)    
-    if args.decoding=='viterbi':
-        Tracts_HMM_mas = run_daiseg_all(Lambda_opt)
-    else:
-        Tracts_HMM_mas=run_daiseg_all_posterior(Lambda_opt, cut_off, gaps_numbers)
+
 
 
 
@@ -197,20 +146,19 @@ with open(args.obs_samples,'r') as f:
     names=f.readlines()
 names=[str(names[i].replace('\n','')) for i in range(len(names))]
 
+
+
 #write into file arg.o results #Sample #Haplotype_number #Archaic tracts
-with open(args.o+'.archaic.txt', "w") as f:
-   for i in range(len(Tracts_HMM_mas)):
-       if i % 2 ==0:
-           f.write(names[int(i // 2)]+'\t0\t'+str(Tracts_HMM_mas[i][1])+'\n')
-       else:
-           f.write(names[int(i // 2)]+'\t1\t'+str(Tracts_HMM_mas[i][1])+'\n')      
+with open(args.o+'.archaic.ND1.txt', "w") as f:
+    for i in range(len(Tracts_HMM_mas)):
 
+        f.write(names[int(i // 2)] + '\t'+str(int(i // 2))+'\t'+str(Tracts_HMM_mas[i][1]) + '\n')
 
+with open(args.o+'.archaic.ND2.txt', "w") as f:
+    for i in range(len(Tracts_HMM_mas)):
+        f.write(names[int(i // 2)]  + '\t'+str(int(i // 2))+'\t'+str(Tracts_HMM_mas[i][2]) + '\n')
+        
 with open(args.o+'.modern.txt', "w") as f:
    for i in range(len(Tracts_HMM_mas)):
-       if i % 2 ==0:
-           f.write(names[int(i // 2)]+'\t0\t'+str(Tracts_HMM_mas[i][0])+'\n')
-       else:
-           f.write(names[int(i // 2)]+'\t1\t'+str(Tracts_HMM_mas[i][0])+'\n')  
-
-
+      
+        f.write(names[int(i // 2)]  + '\t'+str(int(i // 2))+'\t'+str(Tracts_HMM_mas[i][0]) + '\n')
