5c768b2625a4497e791ceed119ad6bb112011c72
Egor Lappo <lappo@stanford.edu>
2026-08-05 16:40:49 +1000

    add PACKAGE/APP to flake.nix

--- flake.nix
+++ flake.nix
@@ -18,8 +18,63 @@
       system:
       let
         pkgs = import nixpkgs { inherit system; };
+        daiseg = pkgs.python3Packages.buildPythonApplication {
+          pname = "daiseg";
+          version = "0.1.0";
+          pyproject = true;
+          src = self;
+          build-system = [ pkgs.python3Packages.setuptools ];
+
+          dependencies = with pkgs.python3Packages; [
+            numpy
+            scipy
+            pandas
+            numba
+            pysam
+            jq
+          ];
+
+          # add stuff we call from subprocess to PATH
+          makeWrapperArgs = [
+            "--prefix PATH : ${
+              pkgs.lib.makeBinPath (
+                with pkgs;
+                [
+                  bcftools
+                  bedtools
+                  jq
+                  gzip
+                  findutils
+                  gnused
+                  coreutils
+                ]
+              )
+            }"
+          ];
+
+          pythonImportsCheck = [ "daiseg.daiseg" ];
+          doCheck = false;
+
+          meta = {
+            description = "DAIseg archaic segment inference";
+            homepage = "https://github.com/Genomics-HSE/DAIseg";
+            mainProgram = "daiseg";
+            platforms = pkgs.lib.platforms.unix;
+          };
+        };
       in
       {
+
+        packages = {
+          inherit daiseg;
+          default = daiseg;
+        };
+
+        apps.default = {
+          type = "app";
+          program = "${daiseg}/bin/daiseg";
+        };
+
         devShells.default =
           with pkgs;
           mkShell {
